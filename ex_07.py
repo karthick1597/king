cd1d811672a072233c76aaae140ab5778042fae7
karthick1597 <63631331+karthick1597@users.noreply.github.com>
2020-04-20 20:55:58 +0530

    Create ex_07.py

---
+++ ex_07.py
@@ -0,0 +1,16 @@
+x=20
+if x<2:
+    print('small')
+elif x<10:
+    print('medium')
+else:
+    print('large')
+print('All done')
+
+x=5
+if x<2:
+    print('small')
+elif x<10:
+    print('medium')
+print('all done')
+
