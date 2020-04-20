8d9b33c988197351adfd7563529e0af72362cd62
karthick1597 <63631331+karthick1597@users.noreply.github.com>
2020-04-20 20:47:15 +0530

    Create ex_04.py

---
+++ ex_04.py
@@ -0,0 +1,16 @@
+x=5
+print("before 5")
+if x==5:
+    print('Is 5')
+    print('Is still 5')
+    print('Third 5')
+print('afterwards 5')
+print('before 6')
+
+if x==6:
+    print('Is 6')
+    print('Is still 6')
+    print('Third 6')
+print('Afterwards 6')
+
+    
